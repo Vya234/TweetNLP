6876c8e0fd10ae480380e465a86d5574916e45b0
Vya234 <kavyarai2561@kgpian.iitkgp.ac.in>
2026-04-10 13:56:53 +0530

    Enhance training pipeline with model selection, batching, and separate model saving

--- train.py
+++ train.py
@@ -13,14 +13,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
 
-# PATH HANDLING
+# PATH
 if os.path.exists("/kaggle/input"):
     BASE_PATH = "/kaggle/input/datasets/vya0651/tweetnlp-project-kavya/TweetNLP/dataset"
 else:
     BASE_PATH = "dataset"
 
 
-# 1. Load data
+# Load data
 train_texts, train_labels = load_data(
     os.path.join(BASE_PATH, "train_text.txt"),
     os.path.join(BASE_PATH, "train_labels.txt")
@@ -32,17 +32,17 @@ val_texts, val_labels = load_data(
 )
 
 
-# 2. Build vocab
+# Build vocab
 vocab = build_vocab(train_texts)
 vocab_size = len(vocab) + 1
 
 
-# 3. Encode
+# Encode
 train_encoded = [encode_text(t, vocab) for t in train_texts]
 val_encoded = [encode_text(t, vocab) for t in val_texts]
 
 
-# 4. Padding
+# Padding
 def pad_sequences(sequences, max_len):
     padded = []
     for seq in sequences:
@@ -59,7 +59,7 @@ train_padded = pad_sequences(train_encoded, max_len)
 val_padded = pad_sequences(val_encoded, max_len)
 
 
-# 5. Tensor
+# Tensor
 X_train = torch.tensor(train_padded).to(device)
 y_train = torch.tensor(train_labels).to(device)
 
@@ -67,23 +67,25 @@ X_val = torch.tensor(val_padded).to(device)
 y_val = torch.tensor(val_labels).to(device)
 
 
-# CHOOSE MODEL
-MODEL_TYPE = "improved"   # "baseline" or "improved"
+# MODEL SELECTION
+MODEL_TYPE = "improved"  # "baseline" or "improved"
 
 if MODEL_TYPE == "baseline":
     model = BaselineModel(vocab_size, 100, 128, 3)
+    save_path = "baseline_model.pth"
 else:
     model = ImprovedModel(vocab_size, 100, 128, 3)
+    save_path = "improved_model.pth"
 
 model = model.to(device)
 
 
-# 6. Loss + optimizer
+# Loss + optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 
-# 7. Training
+# Training
 epochs = 5
 batch_size = 64
 
@@ -98,13 +100,10 @@ for epoch in range(epochs):
         y_batch = y_train[i:i+batch_size]
 
         optimizer.zero_grad()
-
         outputs = model(X_batch)
 
         loss = criterion(outputs, y_batch)
-
         loss.backward()
-
         optimizer.step()
 
         total_loss += loss.item()
@@ -112,7 +111,7 @@ for epoch in range(epochs):
     print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
 
 
-# 8. Validation
+# Validation
 model.eval()
 
 with torch.no_grad():
@@ -123,11 +122,6 @@ with torch.no_grad():
     print("Validation Accuracy:", accuracy)
 
 
-# 9. Save model
-if os.path.exists("/kaggle/input"):
-    save_path = "/kaggle/working/model.pth"
-else:
-    save_path = "model.pth"
-
+# Save model
 torch.save(model.state_dict(), save_path)
 print("Model saved at:", save_path)
